2d95f96111a5ea26f0bc1f9a083bb00b656136a5
basim-ajoub <b_asim_star@hotmail.com>
2022-04-07 11:11:25 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,97 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an BlindSide Videos</h1>
+    <p>{{ relatedvideos[0] }}</p>
+    <HelloWorld
+      class="onebox"
+      v-for="project in projects"
+      :key="project.id"
+      :type="project.type"
+      :imgSrc="project.imgsrc"
+      :pstno="project.pstno"
+      :commentpst="project.commentpst"
+      :methods="relatedVideoFunc"
+    />
+    <div id="view-project-box" class="view-project-box">
+      <button @click="closeProjectImage" class="close-btn">X</button>
+      <div class="view-img">
+        <iframe id="big-video" height="320px" width="700px"> </iframe>
+      </div>
+      <p id="view-type"></p>
+      <div id="relatedvideo"></div>
+    </div>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import jsonProjects from "../../projects.json";
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      projects: jsonProjects,
+      relatedvideos: [],
+    };
+  },
+  methods: {
+    relatedVideoFunc: function () {
+      var typeIp = document.getElementById("view-type").innerText;
+      document.getElementById("relatedvideo").innerHTML = "";
+      var no = this.projects.length;
+      for (let i = 0; i <= no; i++) {
+        if (this.projects[i].type == typeIp) {
+          var x = this.projects[i].imgsrc;
+          document.getElementById("relatedvideo").innerHTML +=
+            '<iframe class="related-box" src="' + x + '"  > </iframe>';
+        }
+      }
+    },
+    closeProjectImage: function () {
+      document.getElementById("view-project-box").style.display = "none";
+    },
+  },
+};
+</script>
+<style lang="scss">
+.onebox {
+  float: left;
+}
+h1 {
+  margin-left: 40%;
+}
+.view-project-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(31, 29, 29, 0.836);
+  display: none;
+  .close-btn {
+    position: absolute;
+    width: 6.5%;
+    right: 0%;
+    &:hover {
+      width: 7%;
+    }
+  }
+
+  .view-img {
+    width: 50%;
+    height: 400px;
+    margin-left: 23%;
+    margin-top: 15px;
+
+    img {
+      height: 400px;
+    }
+  }
+}
+.related-box {
+  margin-left: 2%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Blind Side Test</h2>
+    <div class="login-comp">
+      <h2>LogIn</h2>
+      <div class="form-box">
+        <h3>Email</h3>
+        <input v-model="email" type="email" />
+        <h3>Password</h3>
+        <input v-model="password" type="password" />
+
+        <button v-on:click="logIn" class="submit">LogIn</button>
+        <p>"email": "basem2@blindside.com", "password": "123456",</p>
+      </div>
+    </div>
+    <h2>Applicant's Name : Basim Ajoub</h2>
+    <h2>FrameWork : Vue.js</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import logindata from "../../db.json";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      email: "",
+      password: "",
+      database: logindata,
+    };
+  },
+  methods: {
+    logIn() {
+      let result = this.database;
+
+      // this.$router.push({ name: "HomeUser" });
+      let loginInfo = {
+        email: this.email,
+        password: this.password,
+      };
+      var emailDataBase = result.user[0].email;
+      var passwordDataBase = result.user[0].password;
+      if (
+        emailDataBase == loginInfo.email &&
+        passwordDataBase == loginInfo.password
+      ) {
+        localStorage.setItem("user-Info", JSON.stringify(loginInfo));
+        this.$router.push({ name: "about" });
+        window.alert("matched");
+      } else {
+        window.alert("Wrong Password or Email !");
+      }
+
+      window.alert(loginInfo.email);
+    },
   },
 };
 </script>
+<style lang="scss">
+.home {
+  width: 100%;
+}
+h2 {
+  text-align: center;
+}
+.login-comp {
+  width: 30%;
+  height: 400px;
+  margin-top: 70px;
+  background: rgb(190, 187, 187);
+  margin-left: 35%;
+}
+</style>
